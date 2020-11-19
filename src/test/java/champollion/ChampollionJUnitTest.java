2b21052a9908c69c2004e7fe0cba2dc18b8a0aae
MarieCagnasso <marie.cagnasso@etud.univ-jfc.fr>
2020-11-19 11:15:24 +0100

    test unitaire enseignant.

--- src/test/java/champollion/ChampollionJUnitTest.java
+++ src/test/java/champollion/ChampollionJUnitTest.java
@@ -1,17 +1,27 @@
 package champollion;
 
 import org.junit.jupiter.api.*;
+
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ChampollionJUnitTest {
 	Enseignant untel;
 	UE uml, java;
+	Intervention cm, tp,td;
+	Salle salle;
 		
 	@BeforeEach
 	public void setUp() {
 		untel = new Enseignant("untel", "untel@gmail.com");
 		uml = new UE("UML");
-		java = new UE("Programmation en java");		
+		java = new UE("Programmation en java");
+		salle = new Salle("B01",30);
+		cm = new Intervention(salle,java,untel,new Date(),2,TypeIntervention.CM);
+		tp = new Intervention(salle,uml,untel,new Date(),4,TypeIntervention.TP);
+		td = new Intervention(salle,uml,untel,new Date(),4,TypeIntervention.TD);
+
 	}
 	
 
@@ -36,5 +46,27 @@ public class ChampollionJUnitTest {
                          "L'enseignant doit maintenant avoir 30 heures prévues pour l'UE 'uml'");		
 		
 	}
+
+	@Test
+	public void testEnSousService (){
+		untel.ajouteEnseignement(uml, 0, 10, 0);
+		assertEquals(true,untel.enSousService(), "L'enseignant est en sous-service");
+		untel.ajouteEnseignement(uml, 0, 182, 0);
+		assertEquals(false,untel.enSousService(), "L'enseignant n'est pas en sous-service");
+		untel.ajouteEnseignement(uml, 0, 10, 0);
+		assertEquals(false,untel.enSousService(), "L'enseignant n'est pas en sous-service");
+	}
+
+	@Test
+	public void testAjouterIntervention(){
+		untel.ajouterIntervention(cm);
+		assertEquals(2*1.5,untel.heuresPlanifiées(), "L'enseignant à un CM de 2h plannifié.");
+		untel.ajouterIntervention(tp);
+		untel.ajouterIntervention(td);
+		assertEquals(2*1.5+4*0.75+4,untel.heuresPlanifiées(), "L'enseignant à un CM de 2h plannifié.");
+
+	}
+
+
 	
 }
