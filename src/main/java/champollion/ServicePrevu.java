fba28239cca4a746dbdc75886848b2f747b5c7f0
MarieCagnasso <marie.cagnasso@etud.univ-jfc.fr>
2020-11-19 09:18:58 +0100

    service prevu

--- src/main/java/champollion/ServicePrevu.java
+++ src/main/java/champollion/ServicePrevu.java
@@ -1,6 +1,37 @@
 package champollion;
 
 public class ServicePrevu {
-	// TODO : implémenter cette classe
+    private  int volumeCM;
+    private  int volumeTD;
+    private  int volumeTP;
 
+    public ServicePrevu(int cm,int td, int tp){
+        this.volumeCM = cm;
+        this.volumeTD = td;
+        this.volumeTP = tp;
+    }
+
+    public int getVolumeCM() {
+        return volumeCM;
+    }
+
+    public void setVolumeCM(int volumeCM) {
+        this.volumeCM = volumeCM;
+    }
+
+    public int getVolumeTD() {
+        return volumeTD;
+    }
+
+    public void setVolumeTD(int volumeTD) {
+        this.volumeTD = volumeTD;
+    }
+
+    public int getVolumeTP() {
+        return volumeTP;
+    }
+
+    public void setVolumeTP(int volumeTP) {
+        this.volumeTP = volumeTP;
+    }
 }
